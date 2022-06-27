c5cf4bd798fe553c7455ed799552d1a6075fafc3
The Khiem <lekhiem2002@gmail.com>
2022-06-28 01:06:12 +0700

    21h

--- src/Components/Table/Box.jsx
+++ src/Components/Table/Box.jsx
@@ -1,21 +1,129 @@
-Box.propTypes = {};
+import Boxc from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Modal from "@mui/material/Modal";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function Box(props) {
-  const { gido } = props;
-  const handleClick = (input) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const { gido, newArr, isActive, data } = props;
+  useEffect(() => {
     const node = document.querySelectorAll(".box-wrap");
-    for (let index = input; index < input + 5; index++) {
-      node[index].classList.add("active");
+    if (isActive) {
+      node[gido].classList.add("active");
     }
-
-    for (let index = input + 24; index < input + 24 + 5; index++) {
-      node[index].classList.add("active");
+  }, [isActive, gido]);
+  const handleClick = (input) => {
+    if (input) {
+      newArr.push(input);
+      console.log(input);
+      setOpen(!open);
     }
+    // for (let index = input + 24; index < input + 29; index++) {
+    //   node[index].classList.add("active");
+    // }
   };
 
   return (
     <>
-      <div className="box-wrap" onClick={() => handleClick(gido)}></div>
+      <Grid item xs={0.5}>
+        <div className="box-wrap" onClick={() => handleClick(data)}>
+          <div className="image-box">
+            {isActive && (
+              <div className="image-box-wrap">
+                <div className="empty">
+                  <div className="icon">
+                    <span class="icon-empty">
+                      <span class="path1"></span>
+                      <span class="path2"></span>
+                      <span class="path3"></span>
+                      <span class="path4"></span>
+                      <span class="path5"></span>
+                      <span class="path6"></span>
+                    </span>
+                  </div>
+                  <span className="info">{data.status.có}</span>
+                </div>
+
+                <div className="no-empty">
+                  <div className="icon">
+                    <span class="icon-not-empty">
+                      <span class="path1"></span>
+                      <span class="path2"></span>
+                      <span class="path3"></span>
+                      <span class="path4"></span>
+                      <span class="path5"></span>
+                      <span class="path6"></span>
+                      <span class="path7"></span>
+                      <span class="path8"></span>
+                      <span class="path9"></span>
+                      <span class="path10"></span>
+                      <span class="path11"></span>
+                      <span class="path12"></span>
+                      <span class="path13"></span>
+                    </span>
+                  </div>
+                  <span className="info">{data.status.chưa}</span>
+                </div>
+
+                <div className="ordering">
+                  <div className="icon">
+                    <span class="icon-ordering">
+                      <span class="path1"></span>
+                      <span class="path2"></span>
+                      <span class="path3"></span>
+                      <span class="path4"></span>
+                      <span class="path5"></span>
+                      <span class="path6"></span>
+                      <span class="path7"></span>
+                      <span class="path8"></span>
+                    </span>
+                  </div>
+                  <span className="info">{data.status.đặt}</span>
+                </div>
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Boxc sx={style}>
+                    {open && (
+                      <div>
+                        <h1>{data.vitri}</h1>
+                        <h2>{data.vitri}</h2>
+                        <button onClick={handleClose}>Cancel</button>
+                      </div>
+                    )}
+                  </Boxc>
+                </Modal>
+              </div>
+            )}
+          </div>
+        </div>
+      </Grid>
     </>
   );
 }
--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,12 +1,181 @@
+import Table1 from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Box from "./Box";
 
 function Table(props) {
   let rows = [];
-  for (let i = 0; i <= 767; i++) {
-    rows.push(<Box gido={i} key={i} />);
+
+  const newArr = [];
+  const array = [
+    {
+      vitri: 1,
+      status: { có: 5, chưa: 2, đặt: 0 },
+      data: {
+        ke1: [
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+        ke2: [
+          "co",
+          "chua",
+          "chu",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+      },
+    },
+
+    {
+      vitri: 2,
+      status: { có: 3, chưa: 7, đặt: 6 },
+      data: {
+        ke1: [
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+        ke2: [
+          "co",
+          "chua",
+          "chu",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+      },
+    },
+
+    {
+      vitri: 4,
+      status: { có: 10, chưa: 2, đặt: 0 },
+      data: {
+        ke1: [
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+        ke2: [
+          "co",
+          "chua",
+          "chu",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+      },
+    },
+
+    {
+      vitri: 7,
+      status: { có: 9, chưa: 12, đặt: 6 },
+      data: {
+        ke1: [
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+        ke2: [
+          "co",
+          "chua",
+          "chu",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+          "co",
+          "chua",
+          "chua",
+        ],
+      },
+    },
+  ];
+
+  for (let i = 0; i <= 527; i++) {
+    let returnData = array.find((el) => el.vitri === i);
+    console.log(returnData);
+    if (returnData) {
+      rows.push(
+        <Box
+          newArr={newArr}
+          data={returnData}
+          gido={i}
+          key={i}
+          isActive={true}
+        />
+      );
+    } else {
+      rows.push(<Box newArr={newArr} gido={i} key={i} />);
+    }
   }
 
-  return <div className="box">{rows}</div>;
+  return (
+    <>
+      <Table1 sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0}>
+          {rows}
+        </Grid>
+      </Table1>
+    </>
+    // <div className="box">{rows}</div>);
+  );
 }
 
 export default Table;
